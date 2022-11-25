07f1f8f77e64aeda8b89ea0c2e731d4a9a6b2002
Patrick M <patrickmatias.silva@hotmail.com>
2022-11-25 18:49:22 +0000

    basic authentication with feedback

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDTO } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable({})
 export class AuthService {
-  constructor (private prisma: PrismaService) {}
-  signup(dto: AuthDTO) {
-    return 'I am signed up.';
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDTO) {
+    // generate the password
+    const hash = await argon.hash(dto.password);
+
+    // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // error code referring to the reuse of a unique field (in this case, email)
+        if (error.code === 'P2002')
+          throw new ForbiddenException('Credentials taken.');
+      }
+      throw error;
+    }
   }
 
-  signin() {
-    return 'I am signed in.';
+  async signin(dto: AuthDTO) {
+    // find the user by email
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Incorrect credentials.');
+
+    // compare passwords
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    // if password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Incorrect password.');
+
+    // send back the user
+    delete user.hash;
+    return user;
   }
 }
