e136f7698211faae1ea4bcc6e74f8eb79955a401
Patrick M <patrickmatias.silva@hotmail.com>
2022-11-25 17:43:41 +0000

    data validation now is made using class validation package

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDTO } from './dto';
 
 @Injectable({})
 export class AuthService {
-  signup() {
+  constructor (private prisma: PrismaService) {}
+  signup(dto: AuthDTO) {
     return 'I am signed up.';
   }
 
