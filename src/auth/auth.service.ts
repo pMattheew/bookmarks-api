1e5dabde6aa8a9e5a4903bbd05e69cfffaf02d4e
Patrick M <patrickmatias.silva@hotmail.com>
2022-11-28 15:30:31 +0000

    access token is now being generated and returned on signin/up proccesses | authentication strategy implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDTO } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(dto: AuthDTO) {
     // generate the password
     const hash = await argon.hash(dto.password);
@@ -20,10 +26,8 @@ export class AuthService {
         },
       });
 
-      delete user.hash;
+      return this.signToken(user.id, user.email);
 
-      // return the saved user
-      return user;
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         // error code referring to the reuse of a unique field (in this case, email)
@@ -49,9 +53,25 @@ export class AuthService {
 
     // if password incorrect throw exception
     if (!pwMatches) throw new ForbiddenException('Incorrect password.');
+    
+    return this.signToken(user.id, user.email);
+  }
 
-    // send back the user
-    delete user.hash;
-    return user;
+  async signToken(userId: number, email: string): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    }
   }
 }
